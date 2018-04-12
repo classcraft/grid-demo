73c4f5d8b53a6e8132fe2d90735f3c0f4da7d906
Pier-Luc Gendreau <pierluc@classcraft.com>
2018-04-12 16:39:41 -0400

    Grid demo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, PureComponent } from "react";
 
-class App extends Component {
+import "./App.css";
+
+class App extends PureComponent {
+  state = {
+    mainWidth: 0,
+    asideWidth: 0
+  };
+
+  componentDidMount() {
+    const main = document.querySelector("main");
+    const aside = document.querySelector("aside");
+
+    window.addEventListener("resize", () => {
+      this.setState({
+        mainWidth: main.getBoundingClientRect().width.toFixed(),
+        asideWidth: aside.getBoundingClientRect().width.toFixed()
+      });
+    });
+  }
+
   render() {
+    const { mainWidth, asideWidth } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1>hello, grid</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Link 1</a>
+            </li>
+            <li>
+              <a href="#">Link 2</a>
+            </li>
+            <li>
+              <a href="#">Link 3</a>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          {[...Array(24)].map((arien, i) => <section>Section {i}</section>)}
+        </main>
+        <aside>
+          <section>Section 1</section>
+          <section>{`${asideWidth}px`}</section>
+          <section>Section 3</section>
+        </aside>
+        <footer>ok, bye</footer>
       </div>
     );
   }
